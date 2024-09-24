d864be21dad6878026e3785e46e9995820063153
charmful0x <charmful0x@gmail.com>
2024-09-25 00:01:49 +0200

    perf: run backfilling in a separate task

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 use crate::utils::backfill::backfill_blocks;
 use crate::utils::server_handlers::{handle_get_block_by_id, handle_weave_gm};
 use axum::{routing::get, Router};
+use tokio::task;
 
 mod utils;
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let _ = backfill_blocks(1000).await;
     let router = Router::new()
         .route("/", get(handle_weave_gm))
         .route("/block/id/:id", get(handle_get_block_by_id));
+
+    task::spawn(async move {
+        let _ = backfill_blocks(1000).await;
+    });
 
     Ok(router.into())
 }
