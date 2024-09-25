9fb93a6657a5d87d630b8f2ce815226b9f58fbec
charmful0x <charmful0x@gmail.com>
2024-09-26 00:16:32 +0200

    feat: /block/hash endpoint & handle PS errors

--- src/utils/server_handlers.rs
+++ src/utils/server_handlers.rs
@@ -1,12 +1,17 @@
-use crate::utils::planetscale::ps_get_block_by_id;
+use crate::utils::planetscale::{ps_get_block_by_id, ps_get_block_by_hash};
 use axum::{extract::Path, response::Json};
-use serde_json::Value;
+use serde_json::{json, Value};
 
 pub async fn handle_weave_gm() -> &'static str {
     "WeaveGM!"
 }
 
 pub async fn handle_get_block_by_id(Path(id): Path<u64>) -> Json<Value> {
-    let tx_object = ps_get_block_by_id(id).await;
+    let tx_object = ps_get_block_by_id(id).await.unwrap_or(json!({"error": "error fetching block from cloud"}));
+    Json(tx_object)
+}
+
+pub async fn handle_get_block_by_hash(Path(hash): Path<String>) -> Json<Value> {
+    let tx_object = ps_get_block_by_hash(hash.as_str()).await.unwrap_or(json!({"error": "error fetching block from cloud"}));
     Json(tx_object)
 }
